fee903dfa4868c7539b5cb09e35c4a2a21394523
kelumsampath <edirisingheemks@gmail.com>
2020-10-05 23:27:42 +0530

    first commit

---
+++ src/RecursionAndBackTracking/Factorial.java
@@ -0,0 +1,15 @@
+package RecursionAndBackTracking;
+
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.println(factorial(6));
+    }
+    public static int factorial(int n) {
+        // base cases: fact of 0 is 1
+        if(n == 0)
+            return 1;
+            // recursive case: multiply n by (n-1) factorial
+        else
+            return n*factorial(n-1);
+    }
+}
