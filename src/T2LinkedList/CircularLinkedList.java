25afedca390b4132e011456dfb206074b5d6d790
kelumsampath <edirisingheemks@gmail.com>
2020-10-07 19:44:14 +0530

    add method implemented

--- src/T2LinkedList/CircularLinkedList.java
+++ src/T2LinkedList/CircularLinkedList.java
@@ -9,4 +9,22 @@ public class CircularLinkedList {
         tail = null;
         length = 0;
     }
+
+    // Adds data to beginning of list.
+    public void add(int data){
+        addToHead(data);
+    }
+
+    // Adds element to head of list
+    public void addToHead(int data){
+        CLLNode temp = new CLLNode(data);
+        if (tail == null) { // first data added
+            tail = temp;
+            tail.setNext(tail);
+        } else { // element exists in list
+            temp.setNext(tail.getNext());
+            tail.setNext(temp);
+        }
+        length++;
+    }
 }
