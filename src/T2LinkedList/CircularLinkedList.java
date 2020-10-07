420c0eeed5514de48aef260ad0497f9d16b0f487
kelumsampath <edirisingheemks@gmail.com>
2020-10-07 19:42:55 +0530

    circular linked list class and constructor created

---
+++ src/T2LinkedList/CircularLinkedList.java
@@ -0,0 +1,12 @@
+package T2LinkedList;
+
+public class CircularLinkedList {
+    protected CLLNode tail;
+    protected int length;
+
+    // Constructs a new circular list
+    public CircularLinkedList(){
+        tail = null;
+        length = 0;
+    }
+}
