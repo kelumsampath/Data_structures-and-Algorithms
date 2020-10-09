45e0c6aed4ee575ee4fc79fcff92bec760e1c699
kelumsampath <edirisingheemks@gmail.com>
2020-10-09 18:55:46 +0530

    assign values to objects

--- src/T2LinkedList/DoublyLinkedList.java
+++ src/T2LinkedList/DoublyLinkedList.java
@@ -13,4 +13,27 @@ public class DoublyLinkedList {
         head.setNext(tail);
         length = 0;
     }
+
+    // Get the value at a given position.
+    public int get(int position) {
+        return Integer.MIN_VALUE;
+    }
+
+    // Find the position of the head value that is equal to a given value.
+    // The equals method us used to determine equality.
+    public int getPosition(int data) {
+        // go looking for the data
+        DLLNode temp = head;
+        int pos = 0;
+        while (temp != null) {
+            if (temp.getData() == data) {
+                // return the position if found
+                return pos;
+            }
+            pos += 1;
+            temp = temp.getNext();
+        }
+        // else return -1
+        return Integer.MIN_VALUE;
+    }
 }
