b91d9a421b20a817235ff5c48796014a4a0182f2
kelumsampath <edirisingheemks@gmail.com>
2020-10-09 18:55:11 +0530

    create empty list

--- src/T2LinkedList/DoublyLinkedList.java
+++ src/T2LinkedList/DoublyLinkedList.java
@@ -5,4 +5,12 @@ public class DoublyLinkedList {
     private DLLNode head;
     private DLLNode tail;
     private int length;
+
+    // Create a new empty list.
+    public DoublyLinkedList() {
+        head = new DLLNode(Integer.MIN_VALUE,null,null);
+        tail = new DLLNode(Integer.MIN_VALUE, head, null);
+        head.setNext(tail);
+        length = 0;
+    }
 }
