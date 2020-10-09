7b255dfc69dbfc2a891dd9d667b4ef73c2336a46
kelumsampath <edirisingheemks@gmail.com>
2020-10-09 18:54:10 +0530

    initiate properties in DLL

--- src/T2LinkedList/DLLNode.java
+++ src/T2LinkedList/DLLNode.java
@@ -1,4 +1,13 @@
 package T2LinkedList;
 
 public class DLLNode {
+    public int data;
+    public DLLNode prev;
+    public DLLNode next;
+
+    public DLLNode(int data) {
+        this.data = data;
+        prev = null;
+        next = null;
+    }
 }
